5382efa266685b28d9c172f8cd63fe2a63dfc258
James Cuzella <james.cuzella@lyraphase.com>
2015-12-28 13:44:38 -0700

    Adding initital files from chef cookbook generate

---
+++ README.md
@@ -0,0 +1,4 @@
+# kodi
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'kodi'
+maintainer 'James Cuzella'
+maintainer_email 'james.cuzella@lyraphase.com'
+license 'gplv3'
+description 'Installs/Configures kodi'
+long_description 'Installs/Configures kodi'
+version '0.1.0'
