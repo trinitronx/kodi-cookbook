a78000cd456c2225f203c5bc0c8e17a9c19df3d7
James Cuzella <james.cuzella@lyraphase.com>
2015-12-28 14:28:52 -0700

    Adding tests for kodi default install

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe 'kodi::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe ppa('team-xbmc/ppa') do
+    it { should exist }
+    it { should be_enabled }
+  end
+
+  describe package('kodi') do
+    it { should be_installed }
   end
 end
