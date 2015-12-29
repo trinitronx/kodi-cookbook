54d8d4767b8ca149fa59bad37b6396bc529f9c34
James Cuzella <james.cuzella@lyraphase.com>
2015-12-28 18:50:04 -0700

    Test installing most kodi-pvr addons from ppa:team-xbmc/ppa

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
 # By default, don't install addons
 # For a list of available addon packages, see:
 #   http://kodi.wiki/view/HOW-TO:Install_Kodi_for_Linux#Installing_PVR_Addons
+# For a full list of available packages in team-xbmc PPA:
+#   https://launchpad.net/~team-xbmc/+archive/ubuntu/ppa/+packages?field.name_filter=kodi-pvr&field.status_filter=published&field.series_filter=
 default['kodi']['addons'] = []
