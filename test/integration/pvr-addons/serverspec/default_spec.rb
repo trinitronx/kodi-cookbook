61980aa070efd8d37aad55510178320ed887aedc
James Cuzella <james.cuzella@lyraphase.com>
2015-12-28 17:00:38 -0700

    Allow testing multiple kodi addons in test-kitchen integration tests

--- test/integration/pvr-addons/serverspec/default_spec.rb
+++ test/integration/pvr-addons/serverspec/default_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
+require 'json'
 
 describe 'kodi::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
+  let(:node) { JSON.parse(IO.read(File.join(ENV["TEMP"] || "/tmp", "kitchen/chef_node.json"))) }
+
   describe ppa('team-xbmc/ppa') do
     it { should exist }
     it { should be_enabled }
@@ -12,7 +15,9 @@ describe 'kodi::default' do
     it { should be_installed }
   end
 
-  describe package('kodi-pvr-mythtv') do
-    it { should be_installed }
+  it "should install any addons listed in node['kodi']['addons']" do
+    node['normal']['kodi']['addons'].each do |addon|
+      expect( package(addon) ).to be_installed
+    end
   end
 end
