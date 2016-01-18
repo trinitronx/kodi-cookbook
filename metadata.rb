edf70b8e80d1acac154562de50ee5eb694f60ef1
James Cuzella <james.cuzella@lyraphase.com>
2016-01-18 15:10:11 -0700

    Ensure apt cookbook >= 2.6.0 is used (for PPA support in apt_repository)

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures kodi'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.1'
 
-depends 'apt'
+depends 'apt', '>= 2.6.0'
 
 provides 'kodi::default'
 recipe 'kodi::default', 'Installs kodi, and optionally any addons specified in attributes'
