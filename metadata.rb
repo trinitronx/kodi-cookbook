d7cd69c23c3636da5a1a96f34fcc603b28b1a392
James Cuzella <james.cuzella@lyraphase.com>
2016-01-18 15:47:47 -0700

    Bump cookbook version to v0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'james.cuzella@lyraphase.com'
 license 'gplv3'
 description 'Installs/Configures kodi'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.3'
 
 depends 'apt', '>= 2.6.0'
 
