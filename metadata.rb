520ad6aa928e96467188045e44905f46297787b6
James Cuzella <james.cuzella@lyraphase.com>
2016-01-18 09:03:00 -0700

    Update metadata.rb with recipe & attributes

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,18 @@ maintainer 'James Cuzella'
 maintainer_email 'james.cuzella@lyraphase.com'
 license 'gplv3'
 description 'Installs/Configures kodi'
-long_description 'Installs/Configures kodi'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
 depends 'apt'
+
+provides 'kodi::default'
+recipe 'kodi::default', 'Installs kodi, and optionally any addons specified in attributes'
+attribute 'kodi/addons',
+  :display_name => 'Kodi Addons',
+  :description => 'An optional list of extra kodi addon packages to install. Accepts an array of package names.  For full list of available packages, see kodi ppa: http://kodi.wiki/view/Official_Ubuntu_PPA',
+  :type => 'array',
+  :required => 'optional',
+  :recipes => [ 'kodi::default' ],
+  :default => "[]"
+
