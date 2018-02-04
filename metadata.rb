06d792589e4cdc2d07ebc979cf18cb4bd96257c6
James Cuzella <james.cuzella@lyraphase.com>
2018-02-04 04:22:02 -0700

    Guard old Chef against source_url, issues_url undefined method errors

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description 'Installs/Configures kodi'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.6'
 
-source_url 'https://github.com/trinitronx/kodi-cookbook'
-issues_url 'https://github.com/trinitronx/kodi-cookbook/issues'
+source_url 'https://github.com/trinitronx/kodi-cookbook'  if respond_to?(:source_url)
+issues_url 'https://github.com/trinitronx/kodi-cookbook/issues'   if respond_to?(:issues_url)
 
 depends 'apt', '>= 2.6.0'
 
