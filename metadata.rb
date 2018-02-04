94f8b03e16e372a8ecc3cb2261ca25a60ca1cacf
James Cuzella <james.cuzella@lyraphase.com>
2018-02-04 04:13:29 -0700

    Bump cookbook version to v0.1.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'james.cuzella@lyraphase.com'
 license 'GPL-3.0+'
 description 'Installs/Configures kodi'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.5'
+version '0.1.6'
 
 source_url 'https://github.com/trinitronx/kodi-cookbook'
 issues_url 'https://github.com/trinitronx/kodi-cookbook/issues'
